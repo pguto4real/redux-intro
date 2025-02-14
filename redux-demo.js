3e95b8ad0264ec5dd89696c2ab4e9b4f67454ce4
oluwamuibiclear <igbokweudoka@gmail.com>
2025-02-13 22:01:58 -0600

    increment

--- redux-demo.js
+++ redux-demo.js
@@ -1,9 +1,12 @@
 const redux = require("redux");
 
 const counterReducer = (state = { counter: 0 }, action) => {
-  return {
-    counter: state.counter + 1,
-  };
+  if (action.type === "increment") {
+    return {
+      counter: state.counter + 1,
+    };
+  }
+  return state;
 };
 const store = redux.createStore(counterReducer);
 
@@ -17,3 +20,4 @@ counterSubscriber = () => {
 
 store.subscribe(counterSubscriber);
 store.dispatch({ type: "increment" });
+store.dispatch({ type: "decrement" });
